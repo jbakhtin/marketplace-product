bbbc69b5ca219b324f730207442842b39b3e0c99
Bakhtin <jurij.bakhtin@contractor.calleridreputation.com>
2025-09-06 23:30:05 +0700

    fix: .gitignore'

---
+++ internal/modules/product/use_case/product.go
@@ -0,0 +1,40 @@
+package use_case
+
+import (
+	"context"
+	"github.com/jbakhtin/marketplace-product/internal/modules/product/domain"
+	"github.com/jbakhtin/marketplace-product/internal/modules/product/ports"
+)
+
+type ProductUseCase struct {
+	logger            ports.Logger
+	productRepository ports.ProductRepository
+}
+
+func NewProductUseCase(
+	logger ports.Logger,
+	productRepository ports.ProductRepository,
+) (ProductUseCase, error) {
+	return ProductUseCase{
+		logger:            logger,
+		productRepository: productRepository,
+	}, nil
+}
+
+func (o *ProductUseCase) GetProductBySKU(ctx context.Context, SKU domain.SKU) (domain.Product, error) {
+	product, err := o.productRepository.GetProductBySKU(ctx, SKU)
+	if err != nil {
+		return domain.Product{}, err
+	}
+
+	return product, nil
+}
+
+func (o *ProductUseCase) GetSKUList(ctx context.Context, startSKU domain.SKU, count int) ([]domain.SKU, error) {
+	skus, err := o.productRepository.GetSKUList(ctx, startSKU, count)
+	if err != nil {
+		return nil, err
+	}
+
+	return skus, nil
+}
