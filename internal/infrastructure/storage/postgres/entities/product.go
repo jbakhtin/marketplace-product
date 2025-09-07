f488de912742c5514b51d31b76ef235f7aaa6671
Bakhtin <jurij.bakhtin@contractor.calleridreputation.com>
2025-09-07 22:18:12 +0700

    fix: unique index to products tablem; change created and updated field types

--- internal/infrastructure/storage/postgres/entities/product.go
+++ internal/infrastructure/storage/postgres/entities/product.go
@@ -1,54 +1,19 @@
 package entities
 
 import (
-	"database/sql"
-	"encoding/json"
-	"fmt"
 	"github.com/jackc/pgx/v5/pgtype"
 
 	"github.com/jbakhtin/marketplace-product/internal/modules/product/domain"
 )
 
-var _ sql.Scanner = &FingerPrint{}
-
-type FingerPrint map[string]any
-
-// Scan - метод, реализующий интерфейс sql.Scanner sql.Out
-func (m *FingerPrint) Scan(value interface{}) error {
-	// Проверяем, что значение является срезом байтов ([]uint8)
-	if value == nil {
-		*m = nil
-		return nil
-	}
-
-	bytes, ok := value.([]byte)
-	if !ok {
-		return fmt.Errorf("expected []byte, received %T", value)
-	}
-
-	// Декодируем срез байтов в map[string]interface{}
-	err := json.Unmarshal(bytes, &m)
-	if err != nil {
-		return fmt.Errorf("error decoding JSON: %w", err)
-	}
-
-	return nil
-}
-
 type Product struct {
-	ID        *int         `json:"id,omitempty" db:"id"`
-	SKU       domain.SKU   `json:"sku" db:"sku"`
-	Name      domain.Name  `json:"name" db:"name"`
-	Price     domain.Price `json:"price" db:"price"`
-	CreatedAt pgtype.Date  `json:"created_at" db:"created_at"`
-	UpdatedAt pgtype.Date  `json:"updated_at" db:"updated_at"`
+	ID        *int             `json:"id,omitempty" db:"id"`
+	SKU       domain.SKU       `json:"sku" db:"sku"`
+	Name      domain.Name      `json:"name" db:"name"`
+	Price     domain.Price     `json:"price" db:"price"`
+	CreatedAt pgtype.Timestamp `json:"created_at" db:"created_at"`
+	UpdatedAt pgtype.Timestamp `json:"updated_at" db:"updated_at"`
 }
-
-//func NewEntity(model models.Session) Session {
-//	return Product{
-//		...
-//	}
-//}
 
 func (s *Product) ToModel() domain.Product {
 	return domain.Product{
